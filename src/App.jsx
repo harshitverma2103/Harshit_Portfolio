681754d771776cc05171416e90daed03fe8b3696
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-08 02:46:57 +0530

    add skill section

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AboutUs from "./components/About";
-import Education from "./components/Education";
+import Title from "./components/common/Title";
+import SkillsSection from "./components/Skills";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Title text1="About" text2="Me" id={1}/>
       <AboutUs />
-      <Education />
+      <Title text1="My" text2="Skills" id={2}/>
+      <SkillsSection/>
     </div>
   );
 }
