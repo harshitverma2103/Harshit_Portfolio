e4568ff54e8fa797bde672e8e90366feecf07a10
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-08 00:30:47 +0530

    basic index styling

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import AboutUs from "./components/About";
+import Education from "./components/Education";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <Education />
+    </div>
+  );
+}
+
+export default App;
