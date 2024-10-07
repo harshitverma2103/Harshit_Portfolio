4f7bdc7246c062bfc24ed32ae962384c320b354c
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-08 00:34:03 +0530

    created about me section

---
+++ src/components/About/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+import Title from "../common/Title";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <div className="about-us-content">
+        <Title text1="About" text2="Me" id={1} />
+        <p>
+          👋 Hi there! I&apos;m Harshit Verma, a Frontend Developer passionate
+          about crafting beautiful web experiences. 🌐 I have expertise in
+          JavaScript, React, HTML, and CSS, and I love bringing ideas to life
+          through code! 💻✨ I also have experience in automation testing using
+          Selenium, ensuring quality in every project. ✅ My journey began with
+          a Bachelor&apos;s degree in Mathematics, giving me a solid analytical
+          foundation. 📚 I enjoy exploring UI design concepts and staying
+          updated on the latest tech trends. 🎨 I believe in the power of
+          collaboration and always welcome new connections! 🤝
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
