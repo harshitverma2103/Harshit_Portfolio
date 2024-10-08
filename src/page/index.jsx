6eb2920a4f76284b5b59220b7dc28afc569f12a4
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-08 16:45:03 +0530

    create a page

---
+++ src/page/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import AboutUs from "../components/About";
+import Title from "../components/common/Title";
+import SkillsSection from "../components/Skills";
+
+const Page = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Title text1="About" text2="Me" id={1} />
+      <AboutUs />
+      <Title text1="My" text2="Skills" id={2} />
+      <SkillsSection />
+    </div>
+  );
+};
+
+export default Page;
