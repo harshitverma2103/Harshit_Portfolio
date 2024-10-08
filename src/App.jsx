8bbec626525d18978e8a4f2c72105338cfd02b9f
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-08 16:46:05 +0530

    import ParticleEffect and page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import AboutUs from "./components/About";
-import Title from "./components/common/Title";
-import SkillsSection from "./components/Skills";
+import ParticleEffect from "./components/Common/Particles";
+import Page from "./page";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Title text1="About" text2="Me" id={1} />
-      <AboutUs />
-      <Title text1="My" text2="Skills" id={2} />
-      <SkillsSection />
+    <div style={{ position: "relative", overflow: "hidden", width: "90vw" }}>
+      <ParticleEffect />
+      <Page />
     </div>
   );
 }
