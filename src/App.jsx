570eb3f24e34a7d7603ce0090275dacf34391dcd
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-08 17:04:12 +0530

    deleted page folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import AboutUs from "./components/About";
+import Title from "./components/Common/Title/index";
+import SkillsSection from "./components/Skills";
 import ParticleEffect from "./components/Common/Particles";
 import Page from "./page";
 
 function App() {
   return (
-    <div style={{ position: "relative", overflow: "hidden"}}>
+    <div style={{ position: "relative", overflow: "hidden" }}>
       <ParticleEffect />
-      <Page />
+      <Navbar />
+      <Hero />
+      <Title text1="About" text2="Me" id={1} />
+      <AboutUs />
+      <Title text1="My" text2="Skills" id={2} />
+      <SkillsSection />
     </div>
   );
 }
