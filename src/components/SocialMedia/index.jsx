59b5f0a271cd817bf7198ffb965aa4f48fd56ae4
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-12 22:17:25 +0530

    Update SocialMedia component with external links data and improve maintainability

--- src/components/SocialMedia/index.jsx
+++ src/components/SocialMedia/index.jsx
@@ -6,39 +6,24 @@ import {
   faInstagram,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
+import socialMediaLinks from "./socialMediaLinks";
 import "./styles.css";
+
+const iconMap = {
+  faGithub,
+  faLinkedin,
+  faInstagram,
+  faTwitter,
+};
 
 const SocialMedia = () => {
   return (
     <div className="social-media">
-      <a
-        href="https://github.com/harshitverma2103"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon icon={faGithub} className="social-icon" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/harshit2103/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
-      </a>
-      <a
-        href="https://www.instagram.com/harshitv.here/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon icon={faInstagram} className="social-icon" />
-      </a>
-      <a
-        href="https://x.com/Harshit56879251"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon icon={faTwitter} className="social-icon" />
-      </a>
+      {socialMediaLinks.map(({ href, icon }, index) => (
+        <a key={index} href={href} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={iconMap[icon]} className="social-icon" />
+        </a>
+      ))}
     </div>
   );
 };
---
+++ src/components/SocialMedia/socialMediaLinks.js
@@ -0,0 +1,20 @@
+const socialMediaLinks = [
+  {
+    href: "https://github.com/harshitverma2103",
+    icon: "faGithub",
+  },
+  {
+    href: "https://www.linkedin.com/in/harshit2103/",
+    icon: "faLinkedin",
+  },
+  {
+    href: "https://www.instagram.com/harshitv.here/",
+    icon: "faInstagram",
+  },
+  {
+    href: "https://x.com/Harshit56879251",
+    icon: "faTwitter",
+  },
+];
+
+export default socialMediaLinks;
