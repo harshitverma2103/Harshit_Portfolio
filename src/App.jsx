e401c3820d5378bb3c0791b97dd05de68af493b4
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-12 18:10:20 +0530

    implement lazy loading for better performance and reduce image size

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import AboutUs from "./components/About";
-import Title from "./components/Common/Title/index";
-import SkillsSection from "./components/Skills";
-import ParticleEffect from "./components/Common/Particles";
-import SocialMedia from "./components/SocialMedia";
-import Projects from "./components/Projects";
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Hero = React.lazy(() => import("./components/Hero"));
+const AboutUs = React.lazy(() => import("./components/About"));
+const Title = React.lazy(() => import("./components/Common/Title"));
+const SkillsSection = React.lazy(() => import("./components/Skills"));
+const ParticleEffect = React.lazy(() =>
+  import("./components/Common/Particles")
+);
+const SocialMedia = React.lazy(() => import("./components/SocialMedia"));
+const Projects = React.lazy(() => import("./components/Projects"));
 
 function App() {
   return (
@@ -16,14 +18,18 @@ function App() {
         <ParticleEffect />
         <Navbar />
         <Hero />
-        <Title text1="About" text2="Me" id={1} />
-        <AboutUs />
-        <Title text1="My" text2="Skills" id={2} />
-        <SkillsSection />
-        <Title text1="Latest" text2="Work" id={3} />
-        <Projects />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Title text1="About" text2="Me" id={1} />
+          <AboutUs />
+          <Title text1="My" text2="Skills" id={2} />
+          <SkillsSection />
+          <Title text1="Latest" text2="Work" id={3} />
+          <Projects />
+        </Suspense>
       </div>
-      <SocialMedia />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SocialMedia />
+      </Suspense>
       <Analytics />
     </div>
   );
