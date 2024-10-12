e9801ad056be905696ec6300bf92f64e0b57e5cd
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-12 21:59:08 +0530

    Enhance mobile responsiveness with added padding and improved project card styles

---
+++ src/components/Projects/ProjectsData.js
@@ -0,0 +1,29 @@
+import myImage1 from "../../assets/box_office.png";
+import myImage2 from "../../assets/college.png";
+import myImage3 from "../../assets/Tic_tac.png";
+
+const projects = [
+  {
+    imageUrl: myImage1,
+    title: "Box Office",
+    liveLink: "https://harshitverma2103.github.io/box-office-app/",
+    sourceCode: "https://github.com/harshitverma2103/box-office-app",
+    tags: ["HTML", "CSS", "JavaScript", "React", "API"],
+  },
+  {
+    imageUrl: myImage2,
+    title: "Edusity",
+    liveLink: "https://edusity-chi.vercel.app/",
+    sourceCode: "https://github.com/harshitverma2103/edusity",
+    tags: ["HTML", "CSS", "JavaScript", "React", "React-Scroll"],
+  },
+  {
+    imageUrl: myImage3,
+    title: "Tic Tac Toe",
+    liveLink: "https://tictactoegamebyharshit.surge.sh/",
+    sourceCode: "https://github.com/harshitverma2103/tictactoe-game",
+    tags: ["HTML", "CSS", "JavaScript", "React"],
+  },
+];
+
+export default projects;
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,42 +1,14 @@
 import React from "react";
 import ProjectCard from "../Common/Card";
-import myImage1 from "../../assets/box_office.png";
-import myImage2 from "../../assets/college.png";
-import myImage3 from "../../assets/Tic_tac.png";
+import projects from "./ProjectsData";
 import "./styles.css";
 
-const Projects = () => {
-  const projects = [
-    {
-      imageUrl: myImage1,
-      title: "Box Office",
-      liveLink: "https://harshitverma2103.github.io/box-office-app/",
-      sourceCode: "https://github.com/harshitverma2103/box-office-app",
-      tags: ["HTML", "CSS", "JavaScript", "React", "API"],
-    },
-    {
-      imageUrl: myImage2,
-      title: "Edusity",
-      liveLink: "https://edusity-chi.vercel.app/",
-      sourceCode: "https://github.com/harshitverma2103/edusity",
-      tags: ["HTML", "CSS", "JavaScript", "React", "React-Scroll"],
-    },
-    {
-      imageUrl: myImage3,
-      title: "Tic Tac Toe",
-      liveLink: "https://tictactoegamebyharshit.surge.sh/",
-      sourceCode: "https://github.com/harshitverma2103/tictactoe-game",
-      tags: ["HTML", "CSS", "JavaScript", "React"],
-    },
-  ];
-
-  return (
-    <div className="project-container">
-      {projects.map((project, index) => (
-        <ProjectCard key={index} {...project} />
-      ))}
-    </div>
-  );
-};
+const Projects = () => (
+  <div className="project-container">
+    {projects.map((project) => (
+      <ProjectCard key={project.title} {...project} />
+    ))}
+  </div>
+);
 
 export default Projects;
