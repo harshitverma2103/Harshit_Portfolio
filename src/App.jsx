15b230c44076ad6b2b3d1449865b7d815ada5cbc
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-12 21:27:41 +0530

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -14,22 +14,24 @@ const Projects = React.lazy(() => import("./components/Projects"));
 function App() {
   return (
     <div>
-      <div style={{ position: "relative", overflow: "hidden" }}>
-        <ParticleEffect />
-        <Navbar />
-        <Hero />
+      <div>
+        <div style={{ position: "relative", overflow: "hidden" }}>
+          <ParticleEffect />
+          <Navbar />
+          <Hero />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Title text1="About" text2="Me" id={1} />
+            <AboutUs />
+            <Title text1="My" text2="Skills" id={2} />
+            <SkillsSection />
+            <Title text1="Latest" text2="Work" id={3} />
+            <Projects />
+          </Suspense>
+        </div>
         <Suspense fallback={<div>Loading...</div>}>
-          <Title text1="About" text2="Me" id={1} />
-          <AboutUs />
-          <Title text1="My" text2="Skills" id={2} />
-          <SkillsSection />
-          <Title text1="Latest" text2="Work" id={3} />
-          <Projects />
+          <SocialMedia />
         </Suspense>
       </div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <SocialMedia />
-      </Suspense>
       <Analytics />
     </div>
   );
